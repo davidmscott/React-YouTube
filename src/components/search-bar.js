7a955e0f9491599e9568409f601a206b62f42db0
davidmscott <dscottm@gmail.com>
2016-11-16 23:02:44 -0700

    Initial commit

---
+++ src/components/search-bar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+// // functional component (do not have state)
+// const SearchBar = () => {
+// 	return <input />
+// };
+
+// class-based component (only class-based components have state)
+class SearchBar extends Component {
+	constructor (props) {
+		super(props); // super calls a method that's already defined on the parent class (Component in this case)
+
+		this.state = {term: ''};
+	}
+
+	render () {
+		return (
+			<div className="search-bar">
+				<input
+				value={this.state.term}
+				onChange={event => this.onInputChange(event.target.value)} />
+			</div>
+		);
+	}
+
+	onInputChange (term) {
+		this.setState({term});
+		this.props.onSearchTermChange(term);
+	}
+
+}
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,53 @@
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search-bar.js';
+import VideoList from './components/video-list.js';
+import VideoDetail from './components/video-detail.js';
+const API_KEY = 'AIzaSyATkpEirOr-WlyMFkemo-e8ND1lPrNcAaU';
+
+// class component: have state, this.props always available, whenever state is changed component and all children components automatically re-render
+// functional component: no state, props available if passed as a parameter
+
+// create a new component. This component should produce some html.
+
+class App extends Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('surfboards');
+
+	}
+
+	videoSearch (term) {
+		YTSearch({key: API_KEY, term}, (videos) => {this.setState({
+			videos,
+			selectedVideo: videos[0]
+		})});
+	}
+
+	render () {
+		const videoSearch = _.debounce(term => {this.videoSearch(term)}, 300);
+
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					videos={this.state.videos}
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+				/>
+			</div>
+		);
+	}
+};
+
+// Take this component's generated html and put it on the page (in the DOM).
+
+ReactDOM.render(<App />, document.querySelector('.container'));
